58210d4ec6d0667a7569d1961dbbcb6230c4f2c5
adamwoolhether <adamwoolhether@gmail.com>
2022-06-03 21:47:13 +0800

    triplets with smaller sum, but return [][3]T

--- 02-TwoPointers/tripletsWithSmallerSum/tripletsWithSmallerSum_test.go
+++ 02-TwoPointers/tripletsWithSmallerSum/tripletsWithSmallerSum_test.go
@@ -23,3 +23,25 @@ func TestTripletWithSmallerSum(t *testing.T) {
 		})
 	}
 }
+
+func TestTripletWithSmallerSumTriplets(t *testing.T) {
+	testCases := []struct {
+		input  []float64
+		target float64
+		output [][3]float64
+	}{
+		{[]float64{-1, 0, 2, 3}, 3, [][3]float64{{-1, 0, 3}, {-1, 0, 2}}},
+		{[]float64{-1, 4, 2, 1, 3}, 5, [][3]float64{{-1, 1, 4}, {-1, 1, 3}, {-1, 1, 2}, {-1, 2, 3}}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("input %v", tc.input), func(t *testing.T) {
+			result := tripletWithSmallerSumTriplets(tc.input, tc.target)
+			for i, v := range result {
+				if v != tc.output[i] {
+					t.Errorf("exp %v, got %v", tc.output[i], v)
+				}
+			}
+		})
+	}
+}
